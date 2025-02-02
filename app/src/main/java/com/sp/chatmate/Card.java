ac042d885199992059448422f015faeaa2dc2d56
jer123se12 <jerickseng06@gmail.com>
2025-02-02 18:21:16 +0800

    finished backend

---
+++ app/src/main/java/com/sp/chatmate/Card.java
@@ -0,0 +1,14 @@
+package com.sp.chatmate;
+
+public class Card{
+    public String lang;
+    public String term;
+    public String definition;
+    public int id;
+    public Card(String lang,String term,String definition,int id){
+        this.lang=lang;
+        this.term=term;
+        this.definition=definition;
+        this.id=id;
+    }
+}
